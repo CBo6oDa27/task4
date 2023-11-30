a2e1892d426074ed63f7f8e360580fc2941a89c8
alexeyzakirov <zakirov.alexey@gmail.com>
2023-11-30 06:36:43 +0300

    add str_func

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
 def make_upper_word(word):
-    """Возвращает слово заглавными буквами из PyCharm"""
+    """Возвращает слово заглавными буквами"""
     return  word.upper()
 
-def capitalize_word(word):
-    """Возвращает слова с первыми заглавными буквами"""
-    capitalized_words = ' '.join(word.capitalize() for word in word.split())
+def capitalize_words(words):
+    """Изменяет первые буквы слов на заглавные"""
+    capitalized_words = ' '.join(word.capitalize() for word in words.split())
     return capitalized_words
